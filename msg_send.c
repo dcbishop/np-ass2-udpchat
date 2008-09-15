9d523f07b9d481ee2a8318a56da97e670922f82c
David C. Bishop <DavidCosburnBishop@gmail.com>
2008-09-15 17:42:53 +1000

    pthread test working.

--- msg_send.c
+++ msg_send.c
@@ -14,9 +14,12 @@
 #define DEFAULT_ADDRESS "225.0.0.1"
 #define MAX_DATA_SIZE 1024
 
+typedef struct thread_data_s {
+   int test;
+} thread_data_t;
+
 void* msg_send(void *arg);
 void* msg_recv(void *arg);
-
 
 int main(int argc, char* argv[]) {
    const unsigned char ttl = 1;
@@ -25,6 +28,9 @@ int main(int argc, char* argv[]) {
    struct sockaddr_in my_addr;
    struct sockaddr_in their_addr;
    char hostname[MAXHOSTNAMELEN];
+   
+   thread_data_t thread_data;
+   thread_data.test = 1;
 
    pthread_t msg_send_tid, msg_recv_tid;
    
@@ -80,30 +86,48 @@ int main(int argc, char* argv[]) {
       exit(1);
    }
    
-   msg_send_result = pthread_create(&msg_send_tid, NULL, msg_send, (void*)sockfd);
+   msg_send_result = pthread_create(&msg_send_tid, NULL, msg_send, (void*)&thread_data);
    if(msg_send_result != 0) {
       perror("pthread_create (send)");
       exit(1);
    }
 
-   msg_recv_result = pthread_create(&msg_recv_tid, NULL, msg_recv, (void*)sockfd);
+   msg_recv_result = pthread_create(&msg_recv_tid, NULL, msg_recv, (void*)&thread_data);
    if(msg_recv_result != 0) {
       perror("pthread_create (recv)");
       exit(1);
    }
-   pthread_detach(msg_send_tid);
-   pthread_detach(msg_recv_tid);
+   
+   pthread_join(msg_send_tid, NULL);
+   pthread_join(msg_recv_tid, NULL);
+   
+   //pthread_detach(msg_send_tid);
+   //pthread_detach(msg_recv_tid);
+   
+   while(1) {
+      printf("Nothing....\n");
+   }
    exit(0);
 }
 
 void* msg_send(void *arg) {
+   thread_data_t* thread_data;
+   thread_data = (thread_data_t*)arg;
    while(1) {
-      printf("SEND\n");
+      if(thread_data->test == 1) {
+      	 printf("SEND\n");
+	 thread_data->test = 0;
+      }
    }
 }
 
 void* msg_recv(void *arg) {
+   thread_data_t* thread_data;
+   thread_data = (thread_data_t*)arg;
    while(1) {
-      printf("RECV\n");
+      if(thread_data->test == 0) {
+      	 printf("RECV\n");
+	 thread_data->test = 1;
+      }
    }
 }
