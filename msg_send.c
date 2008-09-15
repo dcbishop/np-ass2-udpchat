de2a38b6c1936ca4f1f8e9778b95f1116bf25de7
David C. Bishop <DavidCosburnBishop@gmail.com>
2008-09-15 22:53:34 +1000

    Command line arguments now handled.

--- msg_send.c
+++ msg_send.c
@@ -10,7 +10,7 @@
 #include <netdb.h> /* MAXHOSTNAMELEN (Solaris only)*/
 #include <pthread.h>
 
-#define DEFAULT_NAME "The Unnamed One"
+#define DEFAULT_NAME "Sgt. Fury"
 #define DEFAULT_PORT 12345
 #define DEFAULT_ADDRESS "225.0.0.1"
 #define MAX_DATA_SIZE 1024
@@ -33,9 +33,12 @@ void* msg_recv(void *arg);
 int main(int argc, char* argv[]) {
    const unsigned char ttl = 1;
    const int on = 1;
-   int sockfd = -0xDEADC0DE;
+   int sockfd = 0xDEADC0DE;
    struct sockaddr_in my_addr;
    struct sockaddr_in their_addr;
+   
+   char address[MAX_DATA_SIZE] = {'\0'};
+   int port = 0xDEADC0DE;
    
    thread_data_t thread_data;
    thread_data.running = 1;
@@ -43,8 +46,8 @@ int main(int argc, char* argv[]) {
    pthread_t msg_send_tid, msg_recv_tid;
    
    /* return values for pthread */
-   int msg_send_result = -0xDEADC0DE;
-   int msg_recv_result = -0xDEADC0DE;
+   int msg_send_result = 0xDEADC0DE;
+   int msg_recv_result = 0xDEADC0DE;
    
    struct ip_mreq mreq;
    inet_aton(DEFAULT_ADDRESS, &mreq.imr_multiaddr);
@@ -52,21 +55,43 @@ int main(int argc, char* argv[]) {
    
    gethostname(thread_data.hostname, MAXHOSTNAMELEN);
    
-   strncpy(thread_data.username, DEFAULT_NAME, MAX_DATA_SIZE);
+   thread_data.username[0] = '\0';
    
-   int name = -0xDEADC0DE;
+   int name = 0xDEADC0DE;
    while ((name = getopt(argc, argv, "u:r")) != -1) {
       switch(name) {
          case 'u':
             strncpy(thread_data.username, optarg, MAX_DATA_SIZE);
             break;
+         case 'm':
+            strncpy(address, optarg, MAX_DATA_SIZE);
+            break;
          case 'r':
             printf("%s: You have set the r flag which does nothing, thats just swell.\n", argv[0]);
             break;
+         case 'p':
+            port = atoi(optarg);
+            break;
          case '?': 
-            printf("%s: Thats not right...\n", argv[0]);
+            fprintf(stderr, "%s: Thats not right... try again...\n", argv[0]);
             exit(EXIT_FAILURE);
       }
+   }
+   
+   if(thread_data.username[0] == '\0') {
+      strncpy(thread_data.username, DEFAULT_NAME, MAX_DATA_SIZE);
+      printf("%s: Due to your failure to specify a name, you will hence forth be known as '%s'.\n", argv[0], thread_data.username);
+   }
+   
+   if(address[0] == '\0') {
+      strncpy(address, DEFAULT_ADDRESS, MAX_DATA_SIZE);
+      printf("%s: %s, because of your apparent inability to specify a multicast address, %s has been assigned for you.\n", argv[0], thread_data.username, address);
+   }
+   
+   printf("PORT %d\n", port);
+   if(port < 0) {
+      port = 12345;
+      printf("%s: %s, as you have not chosen a port number %d has been set.\n", argv[0], thread_data.username, port);
    }
    
    memset(&my_addr, 0, sizeof(my_addr));
@@ -76,7 +101,7 @@ int main(int argc, char* argv[]) {
 
    their_addr.sin_family = AF_INET;
    their_addr.sin_port = htons(DEFAULT_PORT);
-   inet_aton(DEFAULT_ADDRESS, &their_addr.sin_addr);
+   inet_aton(address, &their_addr.sin_addr);
    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
    thread_data.their_addr = &their_addr;
 
@@ -125,22 +150,40 @@ int main(int argc, char* argv[]) {
    exit(0);
 }
 
+int sendRaw(char* message, thread_data_t* thread_data) {
+   if(sendto(thread_data->sockfd, message, strlen(message), 0, (struct sockaddr *)thread_data->their_addr, sizeof(*thread_data->their_addr)) < 0) {
+      perror("sentto");
+      thread_data->running = 0;
+      return EXIT_FAILURE;
+   }
+   return EXIT_SUCCESS;
+}
+
+int sendMessage(char* message, thread_data_t* thread_data) {
+   char buffer[MAX_DATA_SIZE];
+   snprintf(buffer, MAX_DATA_SIZE-1, "<%s> %s", thread_data->username, message);
+   if(!sendRaw(buffer, thread_data)) {
+      return EXIT_FAILURE;
+   }  
+
+   return EXIT_SUCCESS;
+}
+
 void* msg_send(void *arg) {
    thread_data_t* thread_data;
    thread_data = (thread_data_t*)arg;
-   
+
    char buffer[MAX_DATA_SIZE];
+   snprintf(buffer, MAX_DATA_SIZE-1, "<%s@%s entered chat group>\n", thread_data->username, thread_data->hostname);   
+   sendRaw(buffer, thread_data);
    
    while(thread_data->running) {
       sleep(5);
-    
-      snprintf(buffer, MAX_DATA_SIZE-1, "<%s> %s", thread_data->username, "I am sending!\n");
-      if(sendto(thread_data->sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)thread_data->their_addr, sizeof(*thread_data->their_addr)) < 0) {
-         perror("sentto");
-         thread_data->running = 0;
-         return (void*)EXIT_FAILURE;
-      }
+      sendMessage("Be amazed! For I am sending continious messages at intervals of exactly 5 seconds!\n", thread_data);
    }
+   
+   snprintf(buffer, MAX_DATA_SIZE-1, "<%s@%s leaving chat group>\n", thread_data->username, thread_data->hostname);   
+   sendRaw(buffer, thread_data);
    
    fprintf(stdout, "Send thread, self-termination...\n");
    return (void*)EXIT_SUCCESS;
@@ -150,7 +193,7 @@ void* msg_recv(void *arg) {
    thread_data_t* thread_data;
    thread_data = (thread_data_t*)arg;
    
-   int bytes = -0xDEADC0DE;
+   int bytes = 0xDEADC0DE;
    char buffer[MAX_DATA_SIZE];
   
    while(thread_data->running) {
